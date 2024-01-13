859c9fdd36dc6ace144d836c228429b2103d5a25
goatdog <hungthuan0306@gmail.com>
2024-01-13 20:54:07 +0700

    update-answer-from-gpt

--- api/app.py
+++ api/app.py
@@ -3,14 +3,13 @@ import pandas as pd
 import os, wget, json
 from elasticsearch.exceptions import NotFoundError
 
-# ELASTIC_PASSWORD = "dA9UFqZxZ4uL-*-v=4lH"
-ELASTIC_PASSWORD = "dA9UFqZxZ4uL-*-v=4lH"
-CERT_FINGERPRINT = "ddd5637f667684e20163b3438d55edb5d15b6eff79978e81da7e3f9628ca4b8e"
+ELASTIC_PASSWORD = "KENehWXj_IpXqG5-n7sX"
+CERT_FINGERPRINT = "b9d7dc9c6a701580d91134d1de46db58610e98718a97a9c710ea72cd14e73856"
 index_name = "stellar"
 
 client =  Elasticsearch(
     "https://localhost:9200",
-    
+    # ca_certs="http_ca.crt",
     ssl_assert_fingerprint=CERT_FINGERPRINT,
     basic_auth=("elastic", ELASTIC_PASSWORD)
 ) 
@@ -19,26 +18,26 @@ print(client.info())
 def create_index_by_mapping():
     index_mapping = {
         "properties":{
+            "title_vector":{
+                "type" : "dense_vector",
+                "dims": 1536,
+                "index": "true",
+                "similarity":"cosine"
+            },
+            "content_vector": {
+                "type": "dense_vector",
+                "dims": 1536,
+                "index": "true",
+                "similarity": "cosine"
+        },
             
             "title": {"type": "text"},
-            "content": {
-                "properties": {
-                    # "type": "nested",
-                    "text": {
-                        "type": "text",
-                        "index": "true"
-                    },
-                    "vector": {
-                        "type": "dense_vector",
-                        "dims": 1536,
-                        "index": "true",
-                        "similarity": "cosine"
-                    }
-                }
-                },
+            "content": {"type": "text"},
+            "vector_id": {"type": "long"}
         }
     }
     client.indices.create(index=index_name, mappings=index_mapping)
+
     #index data into elasticSeach
 def dataframe_to_bulk_actions(df):
 
@@ -46,22 +45,10 @@ def dataframe_to_bulk_actions(df):
         yield {
             "_index": index_name,
             "_source": {
-                'title' : row["Title"],
-                'content':{
-                    "text": row["content0"], "vector": using_embedding_model(row['content0']).data[0].embedding
-                }
-                # 'content' : [
-                #     {"text": row["content0"], "vector": using_embedding_model(row['content0']).data[0].embedding},
-                #     {"text": row["content1"], "vector": using_embedding_model(row['content1']).data[0].embedding},
-                #     {"text": row["content2"], "vector": using_embedding_model(row['content2']).data[0].embedding},
-                #     {"text": row["content3"], "vector": using_embedding_model(row['content3']).data[0].embedding},
-                #     {"text": row["content4"], "vector": using_embedding_model(row['content4']).data[0].embedding},
-                #     {"text": row["content5"], "vector": using_embedding_model(row['content5']).data[0].embedding},
-                #     {"text": row["content6"], "vector": using_embedding_model(row['content6']).data[0].embedding},
-                #     {"text": row["content7"], "vector": using_embedding_model(row['content7']).data[0].embedding},
-                #     {"text": row["content8"], "vector": using_embedding_model(row['content8']).data[0].embedding},
-                #     {"text": row["content9"], "vector": using_embedding_model(row['content9']).data[0].embedding}
-                # ],
+                'title' : row["title"],
+                'content' : row["content"],
+                'content_vector' : using_embedding_model(row['content']).data[0].embedding,
+            #  'title_vector' : json.loads(row["content_vector"]),
             }
         }
 def insert_document(data):
@@ -77,36 +64,32 @@ def insert_document(data):
 def search_index_by_query(question, k):
 
     query = using_embedding_model(question).data[0].embedding
-    print(query)
     response = client.search(
-    index = index_name,
-    knn={
-        "field": "content.vector",
-        "query_vector": query,
-        "k": k,
-        "num_candidates": 100,
-        # "inner_hits": {
-        #     "fields": [
-        #        'content.text'
-        #     ]
-        # }
+        index = index_name,
+        knn={
+            "field": "content_vector",
+            "query_vector": query,
+            "k": k,
+            "num_candidates": 100
         }
     )
 
     result = []
     for i in range(k):
-        result.append( response['hits']['hits'][i]['_source']['content']['text'])
+        result.append( response['hits']['hits'][i]['_source']['content'])
     return result
 
 # embedding
 import os
 from openai import AzureOpenAI
 import json
+import csv
+
 def using_embedding_model(input):
     client_ = AzureOpenAI(
-    azure_endpoint = "https://sunhackathon14.openai.azure.com/",
-    api_key="9b2cb1b0bb95439e938ddf43e13aa955",
-    api_version="2023-05-15"
+        azure_endpoint = "https://sunhackathon14.openai.azure.com/",
+        api_key="9b2cb1b0bb95439e938ddf43e13aa955",
+        api_version="2023-05-15"
     )
 
     response = client_.embeddings.create(
@@ -115,28 +98,64 @@ def using_embedding_model(input):
     )
     # truong minh co 127 tin + toi hoc 50 tin -> chatgpt ->
     return response
+def create_answer(context):
+    client_ = AzureOpenAI(
+        azure_endpoint = "https://sunhackathon14.openai.azure.com/",
+        api_key="9b2cb1b0bb95439e938ddf43e13aa955",
+        api_version="2023-05-15"
+    )
+    
+    prompt = f"Trả lời câu hỏi dựa vào dữ liệu được cho ở bên dưới, và nếu câu hỏi không thể trả lời được, hãy trả lời mà không cần dựa vào dữ liệu được cho và nếu vẫn không trả lời được, hãy nói \"Tôi không biết\"\n\nDữ liệu được cho:\n {context}\n\n---\n\nCâu trả lời:"
+    
+    response = client_.chat.completions.create(
+        model="GPT35TURBO",
+        messages=[
+            {'role': 'system', 'content': 'Bạn là một trợ lý tốt bụng.'},
+            {'role': 'user', 'content': prompt}
+        ],
+    )
+    return response.choices[0].message.content
 
-data_path = os.path.join("data", "openai.csv")
+
+data_path = "data\openai.csv"
 data = pd.read_csv(data_path)
-begin = 0
-step = 3
-for i in range(len(data)):
-    for j in range(10):
-        end = min((begin+step),len(data.at[i, 'content']))
-        data.at[i, 'content' + str(j)] = data.at[i, 'content'][begin:end]
-        begin = begin + step
-
+data = pd.DataFrame(data)
+history_path = "data\history.csv"
 print(data)
+while True:
+    history = pd.read_csv(history_path)
+    history = pd.DataFrame(history)
+    last_row = history.iloc[-1]
+    if last_row["role"] == "user":
+        question = last_row["content"] #-> output : ban phai du 127 tin chi -> ok
+        # data = array[vector] : sinh vien phai dang ky 127 tin -> vector[1]
 
-demo = insert_document(data)
+        demo = insert_document(data)
 
-k = 2
-for i in range(100):
-    question = input("Nhap cau hoi:") #-> output : ban phai du 127 tin chi -> ok
-    result = search_index_by_query(question, k)
-    print(result)
+        k = 1
+        result = search_index_by_query(question, 1)
+        print(result)
+
+        context = "Dữ liệu đoạn hội thoại trước đây:\n"
+        for index, row in history.iterrows():
+            if row["content"] != last_row["content"]:
+                context += row["content"]
+                context += "\n"
+        context += f"Dữ liệu được cung cấp thêm: {result[0]}\n"
+        context += f"Câu hỏi: {question}"
+        print(context)
+        
+        answer = create_answer(context)
+        print(answer)
+        
+        with open(history_path, "a") as file:
+            writer = csv.writer(file)
+            fields = ["assistant", answer]
+            writer.writerow(fields)
+            file.close()    
 
     
 # H*gu8-e9l8zcrLFQA*L-
+
 
 
